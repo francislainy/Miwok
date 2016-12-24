c91d4f9bd7f0e3afeba83f653a14f6094fb6cc8f
Francislainy <francislainy.campos@gmail.com>
2016-12-24 10:21:08 +0000

    ListView and adapter replacing LinearLayout.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,9 +2,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -23,21 +22,20 @@ public class NumbersActivity extends AppCompatActivity {
             wordsList.add(word);
         }
 
-        LinearLayout rootview = (LinearLayout) findViewById(R.id.rootView);
-//
-//        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
-//        ListView listView = (ListView) findViewById(R.id.list);
-//        listView.setAdapter(itemsAdapter);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
         /**Add words to textview on the LinearLayout
          *
          */
-        for(String word : wordsList){
-            TextView wordView = new TextView(this);
-            wordView.setText(word);
-            rootview.addView(wordView);
-            Log.v("NumbersActivity", "Word on the list: " + word);
-        }
+//        LinearLayout rootview = (LinearLayout) findViewById(R.id.rootView);
+//        for(String word : wordsList){
+//            TextView wordView = new TextView(this);
+//            wordView.setText(word);
+//            rootview.addView(wordView);
+//            Log.v("NumbersActivity", "Word on the list: " + word);
+//        }
 
 
     }
